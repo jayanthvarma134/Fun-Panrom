6ff07c06d3ad6ee5c7ed62323b8d695114cba884
jayanthvarma134 <jayanthvarma134@gmail.com>
2020-05-22 14:14:31 +0530

    Update main.py
    labels and buttons

--- tkinter_freecodecamp/main.py
+++ tkinter_freecodecamp/main.py
@@ -1,1 +1,13 @@
 from tkinter import *
+root = Tk()
+
+e= Entry(root)
+e.pack()
+def myClick():
+	mylabel = Label(root, text='hello')
+	mylabel.pack()
+
+myButton = Button(root, text='Click here', command=myClick)
+myButton.pack()
+
+root.mainloop()
