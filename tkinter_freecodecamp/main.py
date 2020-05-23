6e835c03755669e863e72323588280a2607dc29c
jayanthvarma134 <jayanthvarma134@gmail.com>
2020-05-23 09:22:58 +0530

    Update main.py
    getting input

--- tkinter_freecodecamp/main.py
+++ tkinter_freecodecamp/main.py
@@ -4,7 +4,7 @@ root = Tk()
 e= Entry(root)
 e.pack()
 def myClick():
-	mylabel = Label(root, text='hello')
+	mylabel = Label(root, text='hello'+ e.get())
 	mylabel.pack()
 
 myButton = Button(root, text='Click here', command=myClick)
